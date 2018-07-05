30abd6132d288e033bc08b3472461b99da189d9d
KacperSierakowski <218608@student.pwr.edu.pl>
2018-07-05 17:41:15 +0200

    prawie koniec

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -29,12 +29,81 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(541, 560);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.updateGra);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(697, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "00";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(165, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 46);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "GAME OVER";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(560, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "WYNIK:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(822, 582);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -12,9 +12,169 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        private List<Circle> Snake = new List<Circle>();// W Liscie bedzie pprzetrzymywany waz
+        private Circle food = new Circle(); // pojedynczym kolkiem bedzie jedzenie weza
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+
+            new Settings();
+
+            gameTimer.Interval = 1000 / Settings.Speed;//,,klatki na sekund,,
+            gameTimer.Tick += updateScreen;
+            gameTimer.Start();
+            startGame();
+
+
+
+        }
+
+        private void startGame()
+        {
+            label3.Visible = false;
+            new Settings();
+            Snake.Clear();
+            Circle glowa = new Circle { X = 10, Y = 4 };
+            Snake.Add(glowa);
+
+            label2.Text = Settings.Score.ToString();
+            generatefood();
+        }
+
+        private void generatefood()
+        {
+        }
+
+        private void updateScreen(object sender, EventArgs e)
+        {
+            if (Settings.GameOver == true)
+            {
+                if (Input.KeyPress(Keys.Enter))
+                {
+                    startGame();
+                }
+            }
+            else
+            {
+                if (Input.KeyPress(Keys.Right) && Settings.direction != Directions.Left)
+                {
+                    Settings.direction = Directions.Right;
+                }
+                else if (Input.KeyPress(Keys.Left) && Settings.direction != Directions.Right)
+                {
+                    Settings.direction = Directions.Left;
+                }
+                else if (Input.KeyPress(Keys.Up) && Settings.direction != Directions.Down)
+                {
+                    Settings.direction = Directions.Up;
+                }
+                else if (Input.KeyPress(Keys.Down) && Settings.direction != Directions.Up)
+                {
+                    Settings.direction = Directions.Down;
+                }
+                movePlayer();
+            }
+            pictureBox1.Invalidate();//odswiez picturebox i wgraj grafiki
+        }
+
+        private void movePlayer()
+        {
+            for (int i= Snake.Count-1;i>=0;i--)
+            {
+                if (i == 0)
+                {//jesli glowa sie porusza ... to reszta ciala za nim
+                    switch (Settings.direction)
+                    {
+                        case Directions.Right:
+                            Snake[i].X++;
+                            break;
+                            case Directions.Up:
+                            Snake[i].Y--;
+                            break;
+
+                        case Directions.Left:
+                            Snake[i].X--;
+                            break;
+                        case Directions.Down:
+                            Snake[i].Y++;
+                            break;
+                    }
+                    //Zakres,zeby waz nie wyszedl poza obszar
+                    int MaksymalnaPozycjaX = pictureBox1.Size.Width / Settings.Width;
+                    int MaksymalnaPozycjaY = pictureBox1.Size.Height / Settings.Height;
+
+                    if (Snake[i].X < 0 || Snake[i].Y < 0 || Snake[i].X > MaksymalnaPozycjaX || Snake[i].Y > MaksymalnaPozycjaY)
+                    {
+                        die();//zakoncz gre,jesli waz zderzy sie ze sciana
+
+                    }
+
+                    if(Snake[0].X==food.X && Snake[0].Y == food.Y)//jak glowa dotknie jedzenia
+                    {
+                        eat();
+                    }
+
+
+                }
+                else
+                {
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            Input.changeState(e.KeyCode, true);
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            Input.changeState(e.KeyCode, false);
+        }
+
+        private void updateGra(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            if (Settings.GameOver==false)
+            {
+                Brush KolorWeza;
+
+                for (int i =0;i<Snake.Count;i++)
+                {
+                    if (i==0)
+                    {//Glowa weza zolta
+                        KolorWeza = Brushes.Yellow;
+                    }
+                    else
+                    {//reszta ciala czarna
+                        KolorWeza = Brushes.Black;
+                    }
+
+                    graphics.FillEllipse(KolorWeza, new Rectangle(Snake[i].X * Settings.Width, Snake[i].Y * Settings.Height, Settings.Width, Settings.Height));
+                    graphics.FillEllipse(Brushes.Blue, new Rectangle(food.X * Settings.Width, food.Y * Settings.Height, Settings.Width, Settings.Height));
+
+                }
+
+            }
+            else
+            {
+                //Jak koniec gry to komunikat
+                string gameOver = "GAME OVER\n" + "Końcowy wynik: " + Settings.Score + "\n Enter - Nowa Gra\n";
+                label3.Text = gameOver;
+                label3.Visible = true;
+
+            }
         }
     }
 }
